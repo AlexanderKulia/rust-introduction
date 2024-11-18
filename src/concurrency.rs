29bfd69ed748ca736d74f6efce3fa21af47c2224
Alexander Kulia <37775912+AlexanderKulia@users.noreply.github.com>
2024-11-18 11:30:07 +0100

    chore: add arc mutex example

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -68,3 +68,31 @@ fn mpsc_example() {
         println!("Got: {received}");
     }
 }
+
+fn thread_safety() {
+    // https://doc.rust-lang.org/book/ch16-03-shared-state.html
+    // https://doc.rust-lang.org/book/ch16-04-extensible-concurrency-sync-and-send.html
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+
+    fn main() {
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
+    }
+}
